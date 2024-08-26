1324e7b9d84c72143651bc8aca1e6d8cb4943e5e
phischa <ph.git@mailbox.org>
2024-08-26 14:03:50 +0200

    dashboard&user component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-/* import { NavbarComponent } from './shared/navbar/navbar.component'; */
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserComponent } from './user/user.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet,/*  NavbarComponent, */ SidebarComponent],
+  imports: [CommonModule, RouterOutlet, SidebarComponent, DashboardComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NgIf } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -8,7 +10,7 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [ NgIf, MatSidenavModule, MatButtonModule, MatToolbarModule, MatIconModule ],
+  imports: [ NgIf, RouterOutlet, RouterLink, MatSidenavModule, MatButtonModule, MatToolbarModule, MatIconModule ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
