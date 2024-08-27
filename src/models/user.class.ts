cc54e9b7ab616879fe4c412175d831a8d8007d22
phischa <ph.git@mailbox.org>
2024-08-27 22:12:48 +0200

    added email to user dialog

--- src/models/user.class.ts
+++ src/models/user.class.ts
@@ -1,15 +1,16 @@
 export class User {
         firstName: string;
         lastName: string;
+        email: string;
         birthDate: number;
         address: string;
-        email!: string;
         postalCode: number;
         city: string;
 
         constructor(obj?: any) {
                 this.firstName = obj ? obj.firstName : '';
                 this.lastName = obj ? obj.lastName : '';
+                this.email = obj ? obj.email : '';
                 this.birthDate = obj ? obj.birthDate : '';
                 this.address = obj ? obj.address : '';
                 this.postalCode = obj ? obj.postalCode : '';
@@ -20,6 +21,7 @@ export class User {
                 return {
                         firstName: this.firstName,
                         lastName: this.lastName,
+                        email: this.email,
                         birthDate: this.birthDate,
                         address: this.address,
                         postalCode: this.postalCode,
